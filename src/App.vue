218f8dfdefdf849e182ffdce8b53243357c29a8e
CodeGlimmer <m15073691589@163.com>
2025-06-24 11:01:26 +0800

    删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,3 @@
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped></style>
-
-<!-- <template>
-  <AllPrediction />
-</template>
-
-<script setup>
-import AllPrediction from './views/PredictorViews/AllPrediction.vue'
-</script>
-
-<style lang="scss" scoped></style> -->
